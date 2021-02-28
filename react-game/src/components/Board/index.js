db00e13d0bca94e0a0176a8d3fd4b167ce86ac1a
VaseninaNastya <dadasiegt@yandex.ru>
2021-02-28 22:58:14 +0300

    feat: feat board state

--- react-game/src/components/Board/index.js
+++ react-game/src/components/Board/index.js
@@ -4,10 +4,20 @@ import s from "./Board.module.scss";
 import Square from "../Square"
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
-
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   render() {
     const status = 'Next player: X';
 
--- react-game/src/components/Square/index.js
+++ react-game/src/components/Square/index.js
@@ -1,27 +1,14 @@
-/*class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-        </button>
-      );
-    }
-  }*/
 
 import React from "react";
 import s from "./Square.module.scss";
-//import classNames from "classnames";
+
 
 class Square extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+
   render() {
     return (
-      <button className={s.square} onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className={s.square} onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
